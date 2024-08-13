6bbbc9fcdbaa7f290511803ab7881b6c8bfd01f9
cheerjihyeon <gimjihyeon111@gmail.com>
2024-08-14 01:10:26 +0900

    [Feat] HeroSection.js 추가

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import * as L from './HeroSection.style'
+import React from "react";
+import * as L from "./HeroSection.style";
+import { useNavigate } from "react-router-dom";
 
 function HeroSection() {
   return (
     <L.Wrapper>
-      <L.ImageContainer src='/img/herosection.png' alt='Background' />
-      <L.Container>한 페이지가 될 수 있게</L.Container>
+      <L.ImageContainer src="/img/herosection.png" alt="Background" />
+      <L.Container>
+        한 페이지가 될 수 있게
+        <L.CenteredContainer>
+          <L.TextStyleNormal>
+            아름다운 청춘의 한 장을
+            <br />
+            함께 써내려가는{" "}
+            <L.TextStyleBold>대국민 참여 캠페인</L.TextStyleBold>
+          </L.TextStyleNormal>
+        </L.CenteredContainer>
+
+        <L.BackgroundContainer>
+          <L.ParticipationText>
+            지금 바로 참여하기 <L.ArrowIcon />
+          </L.ParticipationText>
+        </L.BackgroundContainer>
+        
+        <L.LearnMoreText>더 알아보기</L.LearnMoreText>
+        <L.BelowIcon />
+      </L.Container>
     </L.Wrapper>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- src/components/HeroSection/HeroSection.style.js
+++ src/components/HeroSection/HeroSection.style.js
@@ -1,17 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { ReactComponent as ArrowIconSVG } from "../../assets/material-symbols_arrow-back-ios-new-rounded.svg";
+import { ReactComponent as GroupSVG } from "../../assets/Group.svg";
 
 export const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100vh;
-`
+`;
 
 export const ImageContainer = styled.img`
   width: 100%;
   height: 100vh;
   max-width: 400px;
   box-sizing: border-box;
-`
+`;
 
 export const Container = styled.div`
   position: absolute;
@@ -30,4 +32,67 @@ export const Container = styled.div`
   font-family: UhBeemysen;
   font-weight: bold;
   text-align: center;
-`
+`;
+
+export const CenteredContainer = styled.div`
+  text-align: center;
+`;
+
+export const TextStyleNormal = styled.span`
+  color: white;
+  font-size: 1.17rem;
+  font-weight: 400;
+  line-height: 1.5rem;
+  word-wrap: break-word;
+  display: block;
+`;
+
+export const TextStyleBold = styled.span`
+  font-weight: 700;
+`;
+
+export const BackgroundContainer = styled.div`
+  width: 77%;
+  height: 7%;
+  background: #e5eff2;
+  border-radius: 0.2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ParticipationText = styled.div`
+  width: 100%;
+  height: 100%;
+  color: #454545;
+  font-size: 1rem;
+  font-weight: 700;
+  word-wrap: break-word;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ArrowIcon = styled(ArrowIconSVG)`
+  width: 1rem;
+  height: 1rem;
+  /* 필요에 따라 사이즈를 조절하세요 */
+`;
+
+export const LearnMoreText = styled.div`
+  text-align: center;
+  color: white;
+  font-size: 1rem;
+  font-family: "Pretendard Variable", sans-serif;
+  font-weight: 500;
+  word-wrap: break-word;
+  position: absolute;
+  bottom: 4rem;
+`;
+
+export const BelowIcon = styled(GroupSVG)`
+  width: 2rem;
+  height: 2rem;
+  position: absolute;
+  bottom: 1rem;
+`;
