b2b73bbea4c653be270bd822bc37ca6b752c1578
jiheunkim <ziheun54@gmail.com>
2024-08-13 20:14:08 +0900

    [Design 🎨] 홈페이지 기본세팅

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import App from './App'
+import Home from './pages/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+    ],
   },
 ])
 
