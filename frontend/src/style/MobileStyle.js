4c1484970dea2a31838998b6623e62de00c4fe1d
jiheunkim <ziheun54@gmail.com>
2024-08-13 19:57:29 +0900

    [Chore 🚀]  모바일 화면 기본 세팅

---
+++ frontend/src/style/MobileStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const MobileContainer = styled.div`
+  max-width: 400px; // 개발용 화면 디자인 확인
+  width: 100vw;
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  height: 100vh;
+  // padding-top: env(safe-area-inset-top);
+  // padding-right: env(safe-area-inset-right);
+  // padding-bottom: env(safe-area-inset-bottom);
+  // padding-left: env(safe-area-inset-left);
+`
