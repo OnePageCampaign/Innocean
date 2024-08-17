4795a7a513e182a627bc8d36b0d9162ba841afaf
cheerjihyeon <gimjihyeon111@gmail.com>
2024-08-18 06:39:23 +0900

    [Feat] : HeroSection.js 및 HeroSection.style.js 에 '더 알아보기' 추가 구현

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,46 @@
 import React from "react";
 import * as L from "./HeroSection.style";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 function HeroSection() {
   const navigate = useNavigate();
+  const whiteContainerRef = useRef(null);
+  const grayContainerRef = useRef(null);
+  const youthIndependenceRef = useRef(null);
+  const ExtendedSupportInfoRef = useRef(null);
+  const moreIconRef = useRef(null); // MoreIconSvgContainer를 위한 ref 추가
+  const SupportStatsRef = useRef(null);
+  const IndependenceEducationRef = useRef(null);
+  const SettlementGrantRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const screenHeight = window.innerHeight;
+
+      const checkVisibility = (ref) => {
+        const pos = ref.current.getBoundingClientRect().top;
+        if (pos < screenHeight) {
+          ref.current.style.opacity = 1;
+        }
+      };
+
+      checkVisibility(whiteContainerRef);
+      checkVisibility(grayContainerRef);
+      checkVisibility(youthIndependenceRef);
+      checkVisibility(ExtendedSupportInfoRef);
+      checkVisibility(moreIconRef); // MoreIconSvgContainer에 대한 가시성 검사
+      checkVisibility(SupportStatsRef);
+      checkVisibility(IndependenceEducationRef);
+      checkVisibility(SettlementGrantRef);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <L.Wrapper>
       <L.ImageContainer src="/img/herosection.png" alt="Background" />
@@ -30,16 +67,47 @@ function HeroSection() {
         <L.BelowIcon />
       </L.Container>
 
-      <div className="whiteContainer">
-        <div className="centeredText">
-          <span className="textNormal">
-            매년 2천 명이 넘는
-            <br />
-          </span>
-          <span className="textBold">보호대상아동</span>
-          <span className="textNormal">이 발생하고 있습니다</span>
-        </div>
-      </div>
+      <L.WhiteContainer ref={whiteContainerRef}>
+        <L.TextStyleNormal style={{ color: "black" }}>
+          매년 2천 명이 넘는
+          <br />
+          <L.TextStyleBold>보호대상아동</L.TextStyleBold>이 발생하고 있습니다
+        </L.TextStyleNormal>
+      </L.WhiteContainer>
+
+      <L.GrayContainer ref={grayContainerRef}>
+        <L.YouthIndependence ref={youthIndependenceRef}>
+          만 18세가 되면 양육시설에서 나가
+          <br />
+          독립해야 하는 자립준비청년들
+        </L.YouthIndependence>
+
+        <L.ExtendedSupportInfo ref={ExtendedSupportInfoRef}>
+          보호기간이 만 24세까지 연장할 수 있는 법안이 개정됐지만
+          <br />
+          여전히 많은 이들이 만 18세에 나와야 하는 현실 속에 있습니다
+        </L.ExtendedSupportInfo>
+
+        <L.MoreIconSvgContainer ref={moreIconRef} />
+
+        <L.SupportStats ref={SupportStatsRef}>
+          자립지원전담인력 1인당
+          <br />
+          담당 인원 평균 70.8명
+        </L.SupportStats>
+
+        <L.IndependenceEducation ref={IndependenceEducationRef}>
+          퇴소 직전에
+          <br />
+          급하게 하는 자립 교육
+        </L.IndependenceEducation>
+
+        <L.SettlementGrant ref={SettlementGrantRef}>
+          최대 2,000만원
+          <br />
+          자립정착금
+        </L.SettlementGrant>
+      </L.GrayContainer>
     </L.Wrapper>
   );
 }
--- src/components/HeroSection/HeroSection.style.js
+++ src/components/HeroSection/HeroSection.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ReactComponent as ArrowIconSVG } from "../../assets/material-symbols_arrow-back-ios-new-rounded.svg";
 import { ReactComponent as GroupSVG } from "../../assets/Group.svg";
-
+import { ReactComponent as LeaveIconSvg } from "../../assets/보육시설떠나는모습 1.svg";
 export const Wrapper = styled.div`
   position: relative;
   width: 100%;
@@ -97,8 +97,119 @@ export const BelowIcon = styled(GroupSVG)`
   bottom: 1rem;
 `;
 
-export const WhiteContainer = styled.div`
+export const FadeInContainer = styled.div`
+  opacity: 0;
+  transition: opacity 0.8s ease-in-out;
+`;
+
+export const WhiteContainer = styled(FadeInContainer)`
+  width: 100%;
+  height: auto;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding-top: 36px;
+  padding-bottom: 36px;
+  padding-left: 62px;
+  padding-right: 62px;
+`;
+
+export const GrayContainer = styled(FadeInContainer)`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  padding-top: 2rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  justify-content: center;
+  align-items: center;
+  background-color: #3e3e3e;
+  font-size: 3rem;
+  font-family: UhBeemysen;
+  font-weight: bold;
+  text-align: center;
+
+  position: relative;
+`;
+
+export const YouthIndependence = styled(FadeInContainer)`
+  text-align: center;
+  color: white;
+  font-size: 30px; /* 폰트 크기를 30px로 설정 */
+  font-family: "UhBee mysen", sans-serif; /* 폰트 패밀리 설정 */
+  font-weight: 700; /* 폰트 두께를 bold로 설정 */
+  word-wrap: break-word; /* 긴 단어가 다음 줄로 넘어가지 않도록 설정 */
+`;
+
+export const ExtendedSupportInfo = styled(FadeInContainer)`
+  text-align: center;
+  color: white;
+  font-size: 11px; /* 폰트 크기 설정 */
+  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
+  font-weight: 400; /* 폰트 무게 설정 */
+  word-wrap: break-word; /* 단어가 넘치지 않도록 설정 */
+  line-height: 1rem;
+`;
+
+export const MoreIcon = styled.div`
   width: 100%;
   height: 100%;
-  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: opacity 0.8s ease-in-out;
+`;
+export const MoreIconSvgContainer = styled(LeaveIconSvg)`
+  width: 100%;
+  height: auto;
+`;
+
+export const SupportStats = styled(FadeInContainer)`
+  position: absolute;
+  top: 24.5rem;
+  left: 13rem;
+  transform: rotate(3deg); /* 텍스트를 3도 회전 */
+  transform-origin: 0 0; /* 회전의 기준점을 왼쪽 상단으로 설정 */
+  text-align: center; /* 텍스트를 중앙 정렬 */
+  color: white; /* 텍스트 색상을 흰색으로 설정 */
+  font-size: 13px; /* 폰트 크기를 13px로 설정 */
+  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
+  font-weight: 500; /* 폰트 무게를 500으로 설정 */
+  word-wrap: break-word; /* 단어가 넘치지 않도록 설정 */
+  line-height: 1rem;
+`;
+
+export const IndependenceEducation = styled(FadeInContainer)`
+  position: absolute;
+  top: 31rem;
+  left: 0.5rem;
+  transform: rotate(-10deg); /* 텍스트를 -10도 회전 */
+  transform-origin: 0 0; /* 회전의 기준점을 왼쪽 상단으로 설정 */
+  text-align: center; /* 텍스트를 중앙 정렬 */
+  color: white; /* 텍스트 색상을 흰색으로 설정 */
+  font-size: 16px; /* 폰트 크기를 16px로 설정 */
+  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
+  font-weight: 500; /* 폰트 무게를 500으로 설정 */
+  word-wrap: break-word; /* 긴 단어가 다음 줄로 넘어가지 않도록 설정 */
+  line-height: 1.2rem;
+`;
+
+export const SettlementGrant = styled(FadeInContainer)`
+  position: absolute;
+  top: 36rem;
+  left: 16rem;
+  transform: rotate(5deg); /* 텍스트를 5도 회전 */
+  transform-origin: 0 0; /* 회전의 기준점을 왼쪽 상단으로 설정 */
+  text-align: center; /* 텍스트를 중앙 정렬 */
+  color: white; /* 텍스트 색상을 흰색으로 설정 */
+  font-size: 16px; /* 폰트 크기를 16px로 설정 */
+  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
+  font-weight: 500; /* 폰트 무게를 500으로 설정 */
+  word-wrap: break-word; /* 긴 단어가 다음 줄로 넘어가지 않도록 설정 */
+  line-height: 1.2rem;
 `;
