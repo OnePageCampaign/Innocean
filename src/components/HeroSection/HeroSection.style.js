d44cd934bf2408ccafe69efaf6a998c8bf29b11b
jiheunkim <ziheun54@gmail.com>
2024-08-23 05:30:46 +0900

    [Design 🎨] 스크롤 영역 수정

--- src/components/HeroSection/HeroSection.style.js
+++ src/components/HeroSection/HeroSection.style.js
@@ -1,19 +1,19 @@
-import styled from "styled-components";
-import { ReactComponent as ArrowIconSVG } from "../../assets/material-symbols_arrow-back-ios-new-rounded.svg";
-import { ReactComponent as GroupSVG } from "../../assets/Group.svg";
-import { ReactComponent as LeaveIconSvg } from "../../assets/보육시설떠나는모습 1.svg";
+import styled from 'styled-components'
+import { ReactComponent as ArrowIconSVG } from '../../assets/material-symbols_arrow-back-ios-new-rounded.svg'
+import { ReactComponent as GroupSVG } from '../../assets/Group.svg'
+import { ReactComponent as LeaveIconSvg } from '../../assets/보육시설떠나는모습 1.svg'
 export const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100vh;
-`;
+`
 
 export const ImageContainer = styled.img`
   width: 100%;
   height: 100vh;
   max-width: 400px;
   box-sizing: border-box;
-`;
+`
 
 export const Container = styled.div`
   position: absolute;
@@ -32,11 +32,11 @@ export const Container = styled.div`
   font-family: UhBeemysen;
   font-weight: bold;
   text-align: center;
-`;
+`
 
 export const CenteredContainer = styled.div`
   text-align: center;
-`;
+`
 
 export const TextStyleNormal = styled.span`
   color: white;
@@ -45,11 +45,11 @@ export const TextStyleNormal = styled.span`
   line-height: 1.5rem;
   word-wrap: break-word;
   display: block;
-`;
+`
 
 export const TextStyleBold = styled.span`
   font-weight: 700;
-`;
+`
 
 export const BackgroundContainer = styled.div`
   width: 77%;
@@ -59,7 +59,7 @@ export const BackgroundContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 
 export const ParticipationText = styled.div`
   width: 100%;
@@ -71,36 +71,35 @@ export const ParticipationText = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 
 export const ArrowIcon = styled(ArrowIconSVG)`
   width: 1rem;
   height: 1rem;
   /* 필요에 따라 사이즈를 조절하세요 */
-`;
+`
 
 export const LearnMoreText = styled.div`
   text-align: center;
   color: white;
   font-size: 1rem;
-  font-family: "Pretendard Variable", sans-serif;
   font-weight: 500;
   word-wrap: break-word;
   position: absolute;
   bottom: 4rem;
-`;
+`
 
 export const BelowIcon = styled(GroupSVG)`
   width: 2rem;
   height: 2rem;
   position: absolute;
   bottom: 1rem;
-`;
+`
 
 export const FadeInContainer = styled.div`
   opacity: 0;
   transition: opacity 0.8s ease-in-out;
-`;
+`
 
 export const WhiteContainer = styled(FadeInContainer)`
   width: 100%;
@@ -110,11 +109,8 @@ export const WhiteContainer = styled(FadeInContainer)`
   align-items: center;
   justify-content: center;
   text-align: center;
-  padding-top: 36px;
-  padding-bottom: 36px;
-  padding-left: 62px;
-  padding-right: 62px;
-`;
+  padding: 1.5rem;
+`
 
 export const GrayContainer = styled(FadeInContainer)`
   width: 100%;
@@ -122,38 +118,31 @@ export const GrayContainer = styled(FadeInContainer)`
   display: flex;
   flex-direction: column;
   gap: 1.6rem;
+  color: white;
   padding-top: 2rem;
   padding-left: 2rem;
   padding-right: 2rem;
-  justify-content: center;
-  align-items: center;
   background-color: #3e3e3e;
-  font-size: 3rem;
-  font-family: UhBeemysen;
-  font-weight: bold;
-  text-align: center;
-
   position: relative;
-`;
+`
 
 export const YouthIndependence = styled(FadeInContainer)`
   text-align: center;
-  color: white;
-  font-size: 30px; /* 폰트 크기를 30px로 설정 */
-  font-family: "UhBee mysen", sans-serif; /* 폰트 패밀리 설정 */
-  font-weight: 700; /* 폰트 두께를 bold로 설정 */
-  word-wrap: break-word; /* 긴 단어가 다음 줄로 넘어가지 않도록 설정 */
-`;
+  margin-top: 1rem;
+  font-size: 34px;
+  font-family: UhBeemysen;
+  font-weight: bold;
+  word-wrap: break-word;
+`
 
 export const ExtendedSupportInfo = styled(FadeInContainer)`
   text-align: center;
-  color: white;
-  font-size: 11px; /* 폰트 크기 설정 */
-  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
-  font-weight: 400; /* 폰트 무게 설정 */
-  word-wrap: break-word; /* 단어가 넘치지 않도록 설정 */
-  line-height: 1rem;
-`;
+  font-size: 12px;
+  font-weight: 400;
+  word-wrap: break-word;
+  line-height: 1.3rem;
+  z-index: 99;
+`
 
 export const MoreIcon = styled.div`
   width: 100%;
@@ -163,53 +152,49 @@ export const MoreIcon = styled.div`
   align-items: center;
   opacity: 0;
   transition: opacity 0.8s ease-in-out;
-`;
+`
 export const MoreIconSvgContainer = styled(LeaveIconSvg)`
   width: 100%;
-  height: auto;
-`;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`
 
 export const SupportStats = styled(FadeInContainer)`
   position: absolute;
-  top: 400px;
-  left: 14rem;
+  bottom: 280px;
+  left: 230px;
   transform: rotate(3deg); /* 텍스트를 3도 회전 */
   transform-origin: 0 0; /* 회전의 기준점을 왼쪽 상단으로 설정 */
   text-align: center; /* 텍스트를 중앙 정렬 */
-  color: white; /* 텍스트 색상을 흰색으로 설정 */
-  font-size: 13px; /* 폰트 크기를 13px로 설정 */
-  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
+  font-size: 14px; /* 폰트 크기를 13px로 설정 */
   font-weight: 500; /* 폰트 무게를 500으로 설정 */
   word-wrap: break-word; /* 단어가 넘치지 않도록 설정 */
   line-height: 1rem;
-`;
+`
 
 export const IndependenceEducation = styled(FadeInContainer)`
   position: absolute;
-  top: 500px;
-  left: 0.5rem;
+  bottom: 170px;
+  left: 20px;
   transform: rotate(-10deg); /* 텍스트를 -10도 회전 */
   transform-origin: 0 0; /* 회전의 기준점을 왼쪽 상단으로 설정 */
   text-align: center; /* 텍스트를 중앙 정렬 */
-  color: white; /* 텍스트 색상을 흰색으로 설정 */
   font-size: 16px; /* 폰트 크기를 16px로 설정 */
-  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
   font-weight: 500; /* 폰트 무게를 500으로 설정 */
   word-wrap: break-word; /* 긴 단어가 다음 줄로 넘어가지 않도록 설정 */
   line-height: 1.2rem;
-`;
+`
 
 export const SettlementGrant = styled(FadeInContainer)`
   position: absolute;
-  top: 550px;
-  left: 16rem;
+  bottom: 90px;
+  left: 260px;
   transform: rotate(5deg); /* 텍스트를 5도 회전 */
   transform-origin: 0 0; /* 회전의 기준점을 왼쪽 상단으로 설정 */
   text-align: center; /* 텍스트를 중앙 정렬 */
-  color: white; /* 텍스트 색상을 흰색으로 설정 */
   font-size: 16px; /* 폰트 크기를 16px로 설정 */
-  font-family: "Pretendard Variable", sans-serif; /* 폰트 패밀리 설정 */
   font-weight: 500; /* 폰트 무게를 500으로 설정 */
   word-wrap: break-word; /* 긴 단어가 다음 줄로 넘어가지 않도록 설정 */
   line-height: 1.2rem;
-`;
+`
